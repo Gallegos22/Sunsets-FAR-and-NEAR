2be3e5dffc7e8af791a850c79b55af77b32d1d0c
Eduardo Gallegos <gallegoseduardo414@gmail.com>
2024-03-13 01:02:41 +0000

    Working Feature 3

--- js/data.js
+++ js/data.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict";
 let dataObject = {
-  view: '',
-  entries: [],
-  editing: null,
-  nextEntryId: 1,
+    view: '',
+    entries: [],
+    editing: null,
+    nextEntryId: 1,
 };
 window.addEventListener('beforeunload', () => {
-  const sunsetsJSON = JSON.stringify(dataObject);
-  localStorage.setItem('sunsets-local-storage', sunsetsJSON);
+    const sunsetsJSON = JSON.stringify(dataObject);
+    localStorage.setItem('sunsets-local-storage', sunsetsJSON);
 });
 const previousSunsetJSON = localStorage.getItem('sunsets-local-storage');
 if (previousSunsetJSON != null) {
-  dataObject = JSON.parse(previousSunsetJSON);
+    dataObject = JSON.parse(previousSunsetJSON);
 }
